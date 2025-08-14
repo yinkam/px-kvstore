bc62686131834909caadde0c1fa6a72491628c8a
Yinka Makanjuola <yinka.makanjuola@gmail.com>
2025-08-14 09:36:48 +0100

    create skeletal store request handler

--- server.py
+++ server.py
@@ -1,9 +1,34 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+from key_value_store import KeyValueStore
+
+
+class KeyValueStoreHandler(BaseHTTPRequestHandler):
+    """
+    HTTP request handler for the key-value store API.
+    """
+
+    def __init__(self, *args, store=None, **kwargs):
+        self.store = store or KeyValueStore()
+        super().__init__(*args, **kwargs)
+
+
+    def do_GET(self):
+        pass
+
+    def do_POST(self):
+        pass
+
+    def do_PUT(self):
+        pass
+
+    def do_DELETE(self):
+        pass
+
 
 def run(server_class=HTTPServer, port=8000):
     server_address = ('', port)
-    httpd = server_class(server_address, BaseHTTPRequestHandler)
+    httpd = server_class(server_address, KeyValueStoreHandler)
     print(f'Starting httpd on port {port}...')
     httpd.serve_forever()
 
